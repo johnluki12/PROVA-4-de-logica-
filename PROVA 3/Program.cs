7596dff383a99a9c1c76b14f3014e72a19f1fe74
joaolucastbr@gmail.com <joaolucastbr@gmail.com>
2022-07-06 17:58:43 -0300

    Adicionar arquivos de projeto.

---
+++ PROVA 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROVA_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            for (i = 0; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
